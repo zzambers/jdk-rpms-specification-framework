fb65ead5a04aedcec4d900105af4764be174c904
Jana Drazanova <jfabriko@redhat.com>
2016-02-29 21:47:30 +0100

    Add main.py with the main method for framework
    The created file main.py contains a main method that takes
    arguments -h/--help (automatically created by argparse),
    -v/--version.

---
+++ main.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+"""Main entry point to jdks_specification_framework."""
+
+import sys
+import argparse
+
+def createParser():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument( "-v", "--version",
+        help="display the version of the framework",
+        action="store_true" )
+
+    return parser
+
+def runTasks( args ):
+    if( args.version ):
+        print( "jdks_specification_framework, version 0.1" )
+        return
+
+def main( argv ):
+    parser = createParser()
+    args = parser.parse_args()
+    if( len( argv ) < 1 ):
+        parser.print_help()
+    else:
+        runTasks( args )
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
