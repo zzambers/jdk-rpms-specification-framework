31da83df12331acadee1cfad69270a49c36943c0
Jiri Vanek <jvanek@redhat.com>
2016-04-08 15:14:56 +0200

    Logfile and rpm's dir made setupable from commandline

---
+++ config/runtime_config.py
@@ -0,0 +1,48 @@
+import outputControl.logging_access
+
+VERSION_STRING = "jdks_specification_framework, version 0.1"
+
+
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class RuntimeConfig(metaclass=Singleton):
+    pass;
+
+    def __init__(self):
+        self.pkgsDir = "rpms"
+        self.logsFile = "jsf.log"
+
+    def setLogsFile(self, nwFile):
+        oldValue = self.logsFile;
+        self.logsFile = nwFile
+        outputControl.logging_access.LoggingAccess().log("Logfile set to " + nwFile + " instead of " + oldValue)
+
+    def getLogsFile(self):
+        return self.logsFile;
+
+    def setPkgsDir(self, nwDir):
+        outputControl.logging_access.LoggingAccess().log("Rpms looked for in " + nwDir + " instead of " + self.pkgsDir)
+        self.pkgsDir = nwDir
+
+    def getPkgsDir(self):
+        return self.pkgsDir;
+
+    def setFromParser(self, args):
+        # logfile must go first
+        if args.logfile:
+            self.setLogsFile(args.logfile)
+        if args.version:
+            outputControl.logging_access.LoggingAccess().stdout(VERSION_STRING)
+            return False;
+        # later it does not meter
+        outputControl.logging_access.LoggingAccess().log(VERSION_STRING)
+        if args.dir:
+            self.setPkgsDir(args.dir)
+        return True
--- main.py
+++ main.py
@@ -3,23 +3,19 @@
 """Main entry point to jdks_specification_framework."""
 
 import sys
-import config.global_config
-import config.general_parser
 
-from outputControl.logging_access import LoggingAccess
+import config.general_parser
+import config.runtime_config
 
 
 def runTasks(args):
-    if (True | args.version):
-        LoggingAccess().stdout("jdks_specification_framework, version 0.1")
-        return
+    print("Running")
 
 
 def main(argv):
     args = config.general_parser.GeneralParser().parser.parse_args()
-    if len(argv) < 0:
-        config.general_parser.GeneralParser().parser.print_help()
-    else:
+    canContinue = config.runtime_config.RuntimeConfig().setFromParser(args)
+    if canContinue:
         runTasks(args)
 
 
