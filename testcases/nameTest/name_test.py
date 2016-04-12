cfc295920bb0369c20713bec3892d6f76590b91a
Jiri Vanek <jvanek@redhat.com>
2016-04-12 15:28:45 +0200

    regex check made vendor/version depndent and self descibing

--- testcases/nameTest/name_test.py
+++ testcases/nameTest/name_test.py
@@ -8,12 +8,17 @@ import config.global_config as gc
 import config.runtime_config
 import testcases.utils.pkg_name_split as split
 import testcases.utils.base_test
+from testcases.utils.configuration_specific import JdkConfiguration
+from outputControl import logging_access as la
 
-JAVA_REGEX="^java-(1\.[5-8]\.[0-9])|([1-9][0-9]*)-.*-.*-.*\..*.rpm$"
+JAVA_REGEX8="^java-(1\.[5-8]\.[0-9])-.*-.*-.*\..*.rpm$"
 # java-1.X.0 or just 9-whatever1-whatever2-whatever3.whatever4.rpm
 # w1 = vendor, w2 = version
 # w3 = release, w4 = arch .rpm
-CRES_JAVA_REGEXE = re.compile(JAVA_REGEX)
+CRES_JAVA_REGEXE8 = re.compile(JAVA_REGEX8)
+
+JAVA_REGEX9="^java-([1-9][0-9]*)-.*-.*-.*\..*.rpm$"
+CRES_JAVA_REGEXE9 = re.compile(JAVA_REGEX9)
 
 ITW_REGEX="^icedtea-web-.*-.*\..*.rpm$"
 CRES_ITW_REGEXE = re.compile(ITW_REGEX)
@@ -26,26 +31,44 @@ def aInB(a, b):
 def aNotNone(a, b):
     return a is not None
 
-
-def aMatchesB(a, b):
-    if (config.runtime_config.RuntimeConfig().getRpmList().getVendor() == gc.ITW):
-        b2= CRES_ITW_REGEXE.match(a)
-        return  b2
-    else:
-        b1 = CRES_JAVA_REGEXE.match(a)
-        return b1
-
-
 def justCopy(a):
     return a
 
 
+class ItwRegexCheck(JdkConfiguration):
+
+    def checkRegex(self, name=None):
+        self._document("RPM of ITW must match following regex: "+ITW_REGEX)
+        la.LoggingAccess().log("ITW special call for checkRegex")
+        return CRES_ITW_REGEXE.match(name)
+
+
+class Jdk9RegexCheck(JdkConfiguration):
+
+    def checkRegex(self, name=None):
+        self._document("RPM of newer then 8 (except itw) must match following regex: "+JAVA_REGEX9)
+        la.LoggingAccess().log("JDK 9 special call for checkRegex")
+        return CRES_JAVA_REGEXE9.match(name)
+
+
+class OthersRegexCheck(JdkConfiguration):
+
+    def checkRegex(self, name=None):
+        self._document("RPM of jdks older then 9 and except itw must match following regex: "+JAVA_REGEX8)
+        la.LoggingAccess().log("non ITW jdk older then 9 call for checkRegex")
+        return CRES_ITW_REGEXE.match(name)
+
+
 class NameTest(testcases.utils.base_test.BaseTest):
+
+    def aMatchesB(self, a, b):
+            return self.csch.checkRegex(a)
+
     def checkFilesAgainstValues(self, values, function):
         return self.checkFilesAgainstComparator(values, function, aInB)
 
     def checkWholeName(self):
-        return self.checkFilesAgainstComparator(None, justCopy, aMatchesB)
+        return self.checkFilesAgainstComparator(None, justCopy, self.aMatchesB)
 
     def checkFilesAgainstNone(self, function):
         return self.checkFilesAgainstComparator(None, function, aNotNone)
@@ -61,7 +84,7 @@ class NameTest(testcases.utils.base_test.BaseTest):
                 self.log("... is ok")
             else:
                 self.log("... is BAD")
-                failed = failed + 1
+                failed += 1
         return failed
 
     def test_prefix(self):
@@ -84,7 +107,7 @@ class NameTest(testcases.utils.base_test.BaseTest):
         failed = self.checkFilesAgainstNone(split.get_package_name)
         assert failed == 0
 
-    def test_version(self):
+    def test_minorVersion(self):
         failed = self.checkFilesAgainstNone(split.get_minor_ver)
         assert failed == 0
 
@@ -111,6 +134,19 @@ class NameTest(testcases.utils.base_test.BaseTest):
         """
         failed1 = self.checkWholeName()
         assert failed1 == 0
+
+    def setCSCH(self):
+        if config.runtime_config.RuntimeConfig().getRpmList().getJava() == gc.ITW:
+            self.log("Set ItwRegexCheck")
+            self.csch = ItwRegexCheck()
+            return
+        if int(config.runtime_config.RuntimeConfig().getRpmList().getMajorVersionSimplified()) >= 9:
+            self.log("Set Jdk9RegexCheck")
+            self.csch = Jdk9RegexCheck()
+            return
+        self.log("Set OthersRegexCheck")
+        self.csch = OthersRegexCheck()
+
 
 
 def testAll():
