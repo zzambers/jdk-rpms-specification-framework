2305c72aa6b23bc22b764cb80b12f918dcc72b9f
Jiri <jvanek@redhat.com>
2016-04-18 20:29:49 +0200

    Implemneted obsolete test - now for jre.

---
+++ testcases/virtualDeps/obsolete_test.py
@@ -0,0 +1,74 @@
+import sys
+import testcases.utils.core.base_xtest
+import config.runtime_config
+from outputControl import logging_access as la
+from testcases.utils.core.configuration_specific import JdkConfiguration
+import testcases.utils.pkg_name_split as split
+import testcases.utils.rpmbuild_utils as rpmuts
+
+class ITW(JdkConfiguration):
+    def checkJreObsolete(self, obsoletes=None):
+        self._document("Itw obsolete should be aligned to jres' plugins/javawss")
+        # todo - do when doing other plugins
+        pass
+
+class Openjdk8Fedora(JdkConfiguration):
+
+    jreRequiredObsolete=[
+        "java-1.7.0-openjdk",
+        "java-1.5.0-gcj",
+        "sinjdoc"
+    ]
+    def checkJreObsolete(self, obsoletes=None):
+        self._document("jre OpenJdk8 in Fedora must obsolate: " +",".join(Openjdk8Fedora.jreRequiredObsolete))
+        obsoleteJdk7 = 0
+        for obsolete in obsoletes:
+            if obsolete in Openjdk8Fedora.jreRequiredObsolete:
+                obsoleteJdk7 += 1
+        assert obsoleteJdk7 == len(Openjdk8Fedora.jreRequiredObsolete)
+
+
+class JdkRhel(JdkConfiguration):
+    jreExceptionsObsolete = [
+        "java-1.5.0-gcj",
+        "sinjdoc"
+    ]
+    def checkJreObsolete(self, obsoletes=None):
+        self._document("Jdks in rhel must NOT obsolete anything. Possible exceptions: " +",".join(JdkRhel.jreExceptionsObsolete))
+        assert len(set(obsoletes)-set(JdkRhel.jreExceptionsObsolete)) == 0
+
+
+class ObsolateTest(testcases.utils.core.base_xtest.BaseTest):
+
+
+    def test_jreobsolete(self):
+        rpms = self.getBuild()
+        for pkg in rpms:
+            if (split.get_subpackage_only(pkg) == ''):
+                self.csch.checkJreObsolete(rpmuts.listOfVersionlessObsoletes(pkg))
+
+    def setCSCH(self):
+        if config.runtime_config.RuntimeConfig().getRpmList().isItw() :
+            self.csch = ITW()
+            return
+        if config.runtime_config.RuntimeConfig().getRpmList().isFedora():
+            self.csch = Openjdk8Fedora()
+        else:
+            self.csch = JdkRhel()
+
+
+def testAll():
+    return ObsolateTest().execute_tests()
+
+
+def documentAll():
+    la.LoggingAccess().stdout("Jdk obsolates are rhel/fedora techpreview/released depndent.")
+    return ObsolateTest().execute_special_docs()
+
+
+def main(argv):
+    testcases.utils.core.base_xtest.defaultMain(argv, documentAll, testAll)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
