05bcc96141ece71d976edef9fbef3911aa4c6a21
pmikova <pmikova@dhcp-2-105.brq.redhat.com>
2017-05-05 14:36:28 +0200

    Test constants update
    Two methods renamed, a few constants and exclude methods added.

--- utils/test_constants.py
+++ utils/test_constants.py
@@ -17,17 +17,36 @@ WAS_NOT_FOUND = "was not found"
 JAVA_RMI_CGI = "java-rmi.cgi"
 ICED_TEA_PLUGIN_SO = "IcedTeaPlugin.so"
 JMC_INI = "jmc.ini"
+JVM_DIR = "/usr/lib/jvm/"
+SDK_DIRECTORY = "/bin"
+JRE_DIRECTORY = "/jre/bin"
 
 
 # exports jre binaries
-def get_exports_binaries_jre():
+def get_exports_slaves_jre():
     return ["jre_exports", "jre"]
 
 
 # exports sdk binaries
-def get_exports_binaries_sdk():
+def get_exports_slaves_sdk():
     return ["java_sdk_exports", "java_sdk"]
+
+
+def get_ibm_k_bins():
+    return ["klist", "kinit", "ktab"]
+
+
+def get_ibm_ikey_bins():
+    return ["ikeycmd", "ikeyman"]
+
+
+def get_ibm_folders():
+    return ["classic", "j9vm"]
 
 
 def oracle_exclude_list():
     return [JMC_INI]
+
+
+def get_ibm_exclude_list():
+    return get_ibm_k_bins() + get_ibm_folders() + get_ibm_ikey_bins()
