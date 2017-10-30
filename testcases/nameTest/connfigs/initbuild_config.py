5ad6ae177f97156d1fb6af1177e6daa6365f7eff
Petra Alice Mikova <pmikova@redhat.com>
2017-10-30 17:00:26 +0100

    Patched initbuild test config files
    The files were left unnoticed during the big patching, there were some asserts that messed with the tests, replaced them with return statements, deleted two unneccessary line splits (refactoring).

--- testcases/nameTest/connfigs/initbuild_config.py
+++ testcases/nameTest/connfigs/initbuild_config.py
@@ -11,7 +11,7 @@ class ItwVersionCheck(JdkConfiguration):
     def checkMajorVersionSimplified(self, version=None):
         self._document("IcedTea-Web do not have any version contained in name.")
         la.LoggingAccess().log("ITW special call for checkMajorVersionSimplified")
-        assert version == gc.ITW
+        return version == gc.ITW
 
     def checkMajorVersion(self, version=None):
         self.checkMajorVersionSimplified(version)
@@ -19,38 +19,37 @@ class ItwVersionCheck(JdkConfiguration):
     def checkPrefix(self, version=None):
         self._document("IcedTea-Web do not have any prefix-based name. It is just " + gc.ITW + ".")
         la.LoggingAccess().log("ITW special call for checkPrefix")
-        assert version == gc.ITW
+        return version == gc.ITW
 
     def checkVendor(self, vendor=None):
         self._document("IcedTea-Web do not have any vendor in name. It is just " + gc.ITW + ".")
         la.LoggingAccess().log("ITW special call for checkVendor")
-        assert vendor == gc.ITW
+        return vendor == gc.ITW
 
 
 class OthersVersionCheck(JdkConfiguration):
 
     def checkMajorVersionSimplified(self, version=None):
         self._document(
-            "All jdsk (except icedtea-web) have major version in name. eg: java-1.8.0-openjdk or java-9-oracle." \
-            " For legacy JDK (like jdk 1.6.0 or 1.7.1) the major version is included as middle number." \
+            "All jdsk (except icedtea-web) have major version in name. eg: java-1.8.0-openjdk or java-9-oracle."
+            " For legacy JDK (like jdk 1.6.0 or 1.7.1) the major version is included as middle number."
             " For modern jdks (jdk 9+)  the major version is just plain number. Eg.: java-9-ibm."
             " The extracted middle number *is* number")
         la.LoggingAccess().log("non itw call for checkMajorVersionSimplified")
-        assert re.compile("[0-9]+").match(version)
-        assert int(version) > 0
+        return re.compile("[0-9]+").match(version) and int(version) > 0
 
     def checkMajorVersion(self, version=None):
         self._document("The version string in middle of package name is one of: " + ",".join(
             gc.LIST_OF_POSSIBLE_VERSIONS_WITHOUT_ITW))
-        assert version in gc.LIST_OF_POSSIBLE_VERSIONS_WITHOUT_ITW
+        return version in gc.LIST_OF_POSSIBLE_VERSIONS_WITHOUT_ITW
 
     def checkPrefix(self, version=None):
         self._document("prefix of each java package is " + gc.JAVA_STRING + ".")
         la.LoggingAccess().log("non itw call for checkPrefix")
-        assert version == gc.JAVA_STRING
+        return version == gc.JAVA_STRING
 
     def checkVendor(self, vendor=None):
         self._document("The vendor string, suffix of package name is one of: " + ",".join(
             gc.LIST_OF_POSSIBLE_VENDORS_WITHOUT_ITW))
         la.LoggingAccess().log("non itw call for checkVendor")
-        assert vendor in gc.LIST_OF_POSSIBLE_VENDORS_WITHOUT_ITW
+        return vendor in gc.LIST_OF_POSSIBLE_VENDORS_WITHOUT_ITW
