71869b1a44c8af9854082d4387f6f157d7a81c30
jandrlik <jandrlik@redhat.com>
2019-04-10 16:39:14 +0200

    added new version to constants in config, using new method for determining java version number

--- testcases/alternativesTests/binaries_test_paths.py
+++ testcases/alternativesTests/binaries_test_paths.py
@@ -117,7 +117,7 @@ class PathTest(BaseTest):
             if not DefaultMock().postinstall_exception_checked(pkg):
                 self.binaries_test.log("Skipping path test because of missing post install scriptlet.")
                 continue
-            if (_subpkg == DEFAULT or _subpkg == DEFAULT + DEBUG_SUFFIX) and int(pkgsplit.simplify_version(pkgsplit.get_major_ver(name))) >= 10:
+            if (_subpkg == DEFAULT or _subpkg == DEFAULT + DEBUG_SUFFIX) and int(pkgsplit.simplify_full_version(pkgsplit.get_minor_ver(name))) >= 10:
                 self.binaries_test.log("Skipping default package, it has no binaries.")
                 continue
             if _subpkg == PLUGIN and pkgsplit.get_vendor(name) == "ibm" and \
